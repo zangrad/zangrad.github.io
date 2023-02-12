3afbe61fcc7824fd04138b53a390d25060ee0535
zangrad <zan.grad@tecnico.ulisboa.pt>
2023-02-12 19:02:47 +0000

    new deploy 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,8 +8,10 @@ export default defineNuxtConfig({
       ],
       meta: [
         { name: 'description', content: "Doctoral student working on Lie theory, differential geometry and topology in category theory and mathematical physics." },
+        { name: 'keywords', content: 'Lie categories, Lie groupoids, Lie algebroids, differential geometry, differential topology, mathematical physics' },
+        { name: 'author', content: 'Žan Grad' },
         { name: "google-site-verification", content: "5XJiG03RmRFYp12aMDeXTie640ueCk-guBIZq7Dy29A" },
-        { name: "robots", content: "index. follow" }
+        { name: "robots", content: "index. follow" },
       ]
       // script: [{ src: 'https://polyfill.io/v3/polyfill.min.js?features=es6' }],
       // script: [{ id: 'MathJax-script', async: true,  src: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js' }],
