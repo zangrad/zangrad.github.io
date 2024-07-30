ee0b193132d5daea8f8cfbbfe3ab91604204bed1
Žan Grad <zangrad@Zans-Air.home>
2024-07-30 14:54:55 +0200

    Deploy 2024-07-30 14:54

---
+++ .output/public/_nuxt/ProseLi.8a1b1dd8.js
@@ -0,0 +1,1 @@
+import{e as r}from"./entry.afec1746.js";import{a2 as o,a3 as t,ar as a}from"./app.config.7c92b3e1.js";const s={};function n(e,c){return o(),t("li",null,[a(e.$slots,"default")])}const _=r(s,[["render",n]]);export{_ as default};
