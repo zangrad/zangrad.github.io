c605ae17d9fbde73556808096a47eb4d25388994
Žan Grad <zangrad@Zans-MacBook-Air.local>
2024-10-12 01:53:42 +0100

    Deploy 2024-10-12 01:53

---
+++ _nuxt/ProseTbody.b5266be9.js
@@ -0,0 +1,1 @@
+import{e as o}from"./entry.96c02e46.js";import{a2 as r,a3 as t,ar as a}from"./app.config.7c92b3e1.js";const s={};function n(e,c){return r(),t("tbody",null,[a(e.$slots,"default")])}const d=o(s,[["render",n]]);export{d as default};
